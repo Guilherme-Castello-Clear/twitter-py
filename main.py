46ef4a145003a62cf8895426df075a17f2a5f28f
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-11-17 16:21:16 -0300

    Feat: Getting internet speed

--- main.py
+++ main.py
@@ -7,5 +7,35 @@ import time
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
+PROMISED_DOWN = 150
+PROMISED_UP = 10
 TWITTER_EMAIL = "guiauguisto.castello@gmail.com"
 TWITTER_PASSWORD = "nop"
+
+
+class InternetSpeedTwitterBot:
+    def __init__(self, driver_path):
+        self.driver = webdriver.Chrome(options=driver_path)
+        self.up = 0
+        self.down = 0
+
+    def get_internet_speed(self):
+        self.driver.get("https://www.speedtest.net/")
+        time.sleep(3)
+        go_button = self.driver.find_element(By.CSS_SELECTOR,".start-button a")
+        go_button.click()
+        time.sleep(60)
+        self.up = self.driver.find_element(By.XPATH,
+            '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
+        self.down = self.driver.find_element(By.XPATH,
+            '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span').text
+        
+    def tweet_at_provider(self):
+        pass
+
+
+
+
+bot = InternetSpeedTwitterBot(chrome_options)
+bot.get_internet_speed()
+bot.tweet_at_provider()
